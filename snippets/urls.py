becac5c7d5637756050382980c4e48d6ab381954
LirovNursultan <lirov_n@iuca.kg>
2026-02-20 15:51:54 +0600

    Tutorials 3 completed

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic.base import RedirectView
 from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
 
 urlpatterns = [
-    path("snippets/", views.snippet_list),
-    path("snippets/<int:pk>/", views.snippet_detail),
+    path('', RedirectView.as_view(url='/snippets/', permanent=True)),
+    path("snippets/", views.SnippetList.as_view()),
+    path("snippets/<int:pk>/", views.SnippetDetail.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
